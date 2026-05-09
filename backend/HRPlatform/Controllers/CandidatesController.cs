2adc3fc45f4abd11af2a067b83970df65ab0afab
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2026-05-09 19:32:34 +0200

    feat: implement create candidate controller

---
+++ backend/HRPlatform/Controllers/CandidatesController.cs
@@ -0,0 +1,33 @@
+﻿using HRPlatform.Dtos.Candidates;
+using HRPlatform.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/candidates")]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidatesController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCandidateDto dto)
+        {
+            try
+            {
+                var result = await _candidateService.CreateAsync(dto);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
