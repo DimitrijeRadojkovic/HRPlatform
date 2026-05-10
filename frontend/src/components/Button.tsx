ddebd7397a62a9a74b8eb8033f3aa4bb69cbea40
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2026-05-10 21:04:41 +0200

    feat: create home page

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,57 @@
+export default function Button({
+    text,
+    onClick,
+    className,
+    animatedBorder = false
+} : {
+    text: string,
+    onClick: () => void | null,
+    className?: string,
+    animatedBorder?: boolean
+}){
+    return (
+        <div
+            className={`
+                relative
+                rounded-[34px]
+                p-[3px]
+                overflow-hidden
+                transition-all duration-300
+
+                ${
+                    animatedBorder
+                    ? `
+                        before:absolute
+                        before:inset-[-100%]
+                        before:bg-[conic-gradient(from_0deg,#ffffff,#99E3FF,#4A7DFF,#ffffff)]
+                        before:animate-[spin_3s_linear_infinite]
+                        before:content-['']
+
+                        hover:scale-105
+                        hover:shadow-[0_0_35px_rgba(153,227,255,0.7)]
+                    `
+                    : ""
+                }
+            `}
+        >
+            <button
+                onClick={onClick}
+                className={`
+                    relative
+                    z-10
+
+                    flex justify-center items-center
+                    py-[10px] px-[42px]
+
+                    rounded-[30px]
+
+                    transition-all duration-300
+
+                    ${className}
+                `}
+            >
+                {text}
+            </button>
+        </div>
+    )
+}
